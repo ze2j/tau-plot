c0ebd18b42a5220a7b486fc102e98a3cdcdae7b8
Matthieu Lapeyre <46556066+ze2j@users.noreply.github.com>
2026-04-25 09:32:43 +0200

    Add tests for NaN and INF values (#16)

--- addons/tau-plot/tests/bar/nan_or_inf/test_bar_nan_inf.gd
+++ addons/tau-plot/tests/bar/nan_or_inf/test_bar_nan_inf.gd
@@ -1,44 +1,15 @@
 @tool
 extends Control
 
-var _timer: Timer = null
-var _t: float = 0.0
-
-var _datasets: Array[TauPlot.Dataset] = []
-var _state: Array[Dictionary] = []
-
 func _ready() -> void:
-	_create_timer()
-	_setup_all_tests()
-	_timer.start()
-
-func _create_timer() -> void:
-	_timer = Timer.new()
-	_timer.one_shot = false
-	_timer.wait_time = 0.016
-	_timer.timeout.connect(_on_tick)
-	add_child(_timer)
-
-func _setup_all_tests() -> void:
-	_datasets.clear()
-	_state.clear()
-
 	_setup_test_1()
 	_setup_test_2()
 	_setup_test_3()
 	_setup_test_4()
 	_setup_test_5()
 	_setup_test_6()
-
-func _on_tick() -> void:
-	_t += _timer.wait_time
-
-	_step_test_1()
-	_step_test_2()
-	_step_test_3()
-	_step_test_4()
-	_step_test_5()
-	_step_test_6()
+	_setup_test_7()
+	_setup_test_8()
 
 ####################################################################################################
 # Test 1
@@ -46,36 +17,31 @@ func _on_tick() -> void:
 
 func _setup_test_1() -> void:
 	var series_names := PackedStringArray(["A", "B"])
-	var x := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var x := PackedFloat64Array([0.0, NAN, 1.0, INF, 2.0, 2.5])
 	var y_a := PackedFloat64Array([1.0, 2.0, 1.4, 2.2, 1.1, 1.8])
 	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 1.2])
 
 	var dataset := TauPlot.Dataset.make_shared_x_continuous(series_names, x, [y_a, y_b])
-	_datasets.append(dataset)
 
-	%TestPlot1.title = "[GROUPED] z-order = SERIES_ORDER"
+	%TestPlot1.title = "[GROUPED] 2nd and 4th x values are invalid"
 
 	var x_axis := TauAxisConfig.new()
-	x_axis.title = "X"
 	x_axis.type = TauAxisConfig.Type.CONTINUOUS
 	x_axis.scale = TauAxisConfig.Scale.LINEAR
 	x_axis.tick_count_preferred = 8
 
-	var left_axis := TauAxisConfig.new()
-	left_axis.title = "Y"
-	left_axis.type = TauAxisConfig.Type.CONTINUOUS
-	left_axis.scale = TauAxisConfig.Scale.LINEAR
-	left_axis.include_zero_in_domain = true
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
 
 	var bar_config := TauBarConfig.new()
 	bar_config.mode = TauBarConfig.BarMode.GROUPED
-	bar_config.z_order = TauPaneOverlayConfig.ZOrder.SERIES_ORDER
 	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
-	bar_config.bar_width_x_units = 0.25
-	bar_config.bar_gap_x_units = 0.0
+	bar_config.bar_width_x_units = 0.1
 
 	var pane_config := TauPaneConfig.new()
-	pane_config.y_left_axis = left_axis
+	pane_config.y_left_axis = y_axis
 	pane_config.overlays = [bar_config]
 
 	var config := TauXYConfig.new()
@@ -97,56 +63,37 @@ func _setup_test_1() -> void:
 
 	%TestPlot1.plot_xy(dataset, config, bindings)
 
-	_state.append(
-		{
-			"plot": %TestPlot1,
-			"bar_config": bar_config
-		}
-	)
-
-func _step_test_1() -> void:
-	var st := _state[0]
-	var plot: Control = st["plot"]
-	var bar_config: TauBarConfig = st["bar_config"]
-
-	bar_config.bar_width_x_units = clampf(0.25 + 0.25 * sin(_t * 0.5), 0.0, 0.5)
-	plot.refresh_now()
-
 ####################################################################################################
 # Test 2
 ####################################################################################################
 
 func _setup_test_2() -> void:
 	var series_names := PackedStringArray(["A", "B"])
-	var x := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var x := PackedFloat64Array([0.0, NAN, 1.0, INF, 2.0, 2.5])
 	var y_a := PackedFloat64Array([1.0, 2.0, 1.4, 2.2, 1.1, 1.8])
 	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 1.2])
 
 	var dataset := TauPlot.Dataset.make_shared_x_continuous(series_names, x, [y_a, y_b])
-	_datasets.append(dataset)
 
-	%TestPlot2.title = "[STACKED] z-order = SERIES_ORDER"
+	%TestPlot2.title = "[STACKED] 2nd and 4th x values are invalid"
 
 	var x_axis := TauAxisConfig.new()
-	x_axis.title = "X"
 	x_axis.type = TauAxisConfig.Type.CONTINUOUS
 	x_axis.scale = TauAxisConfig.Scale.LINEAR
 	x_axis.tick_count_preferred = 8
 
-	var left_axis := TauAxisConfig.new()
-	left_axis.title = "Y"
-	left_axis.type = TauAxisConfig.Type.CONTINUOUS
-	left_axis.scale = TauAxisConfig.Scale.LINEAR
-	left_axis.include_zero_in_domain = true
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
 
 	var bar_config := TauBarConfig.new()
 	bar_config.mode = TauBarConfig.BarMode.STACKED
-	bar_config.z_order = TauPaneOverlayConfig.ZOrder.SERIES_ORDER
 	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
 	bar_config.bar_width_x_units = 0.4
 
 	var pane := TauPaneConfig.new()
-	pane.y_left_axis = left_axis
+	pane.y_left_axis = y_axis
 	pane.overlays = [bar_config]
 
 	var config := TauXYConfig.new()
@@ -168,57 +115,38 @@ func _setup_test_2() -> void:
 
 	%TestPlot2.plot_xy(dataset, config, bindings)
 
-	_state.append(
-		{
-			"plot": %TestPlot2,
-			"bar_config": bar_config
-		}
-	)
-
-func _step_test_2() -> void:
-	var st := _state[1]
-	var plot: Control = st["plot"]
-	var bar_config: TauBarConfig = st["bar_config"]
-
-	bar_config.bar_width_x_units = clampf(0.4 + 0.4 * sin(_t * 0.6), 0.0, 0.8)
-	plot.refresh_now()
-
 ####################################################################################################
 # Test 3
 ####################################################################################################
 
 func _setup_test_3() -> void:
 	var series_names := PackedStringArray(["A", "B"])
-	var x_a := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var x_a := PackedFloat64Array([0.0, NAN, 1.0, INF, 2.0, 2.5])
 	var x_b := PackedFloat64Array([0.25, 0.75, 1.25, 1.75, 2.25])
 	var y_a := PackedFloat64Array([1.0, 2.0, 1.4, 2.2, 1.1, 1.8])
 	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5])
 
 	var dataset := TauPlot.Dataset.make_per_series_x_continuous(series_names, [x_a, x_b], [y_a, y_b])
-	_datasets.append(dataset)
 
-	%TestPlot3.title = "[INDEPENDENT] z-order = SERIES_ORDER"
+	%TestPlot3.title = "[INDEPENDENT + PER_SERIES_X] 2nd and 4th x values are invalid for series A"
 
 	var x_axis := TauAxisConfig.new()
-	x_axis.title = "X"
 	x_axis.type = TauAxisConfig.Type.CONTINUOUS
 	x_axis.scale = TauAxisConfig.Scale.LINEAR
 	x_axis.tick_count_preferred = 8
 
-	var left_axis := TauAxisConfig.new()
-	left_axis.title = "Y"
-	left_axis.type = TauAxisConfig.Type.CONTINUOUS
-	left_axis.scale = TauAxisConfig.Scale.LINEAR
-	left_axis.include_zero_in_domain = true
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
 
 	var bar_config := TauBarConfig.new()
 	bar_config.mode = TauBarConfig.BarMode.INDEPENDENT
-	bar_config.z_order = TauPaneOverlayConfig.ZOrder.SERIES_ORDER
 	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
-	bar_config.bar_width_x_units = 0.25
+	bar_config.bar_width_x_units = 0.1
 
 	var pane := TauPaneConfig.new()
-	pane.y_left_axis = left_axis
+	pane.y_left_axis = y_axis
 	pane.overlays = [bar_config]
 
 	var config := TauXYConfig.new()
@@ -240,57 +168,37 @@ func _setup_test_3() -> void:
 
 	%TestPlot3.plot_xy(dataset, config, bindings)
 
-	_state.append(
-		{
-			"plot": %TestPlot3,
-			"bar_config": bar_config
-		}
-	)
-
-func _step_test_3() -> void:
-	var st := _state[2]
-	var plot: Control = st["plot"]
-	var bar_config: TauBarConfig = st["bar_config"]
-
-	bar_config.bar_width_x_units = clampf(0.25 + 0.25 * sin(_t * 0.6), 0.0, 0.5)
-	plot.refresh_now()
-
 ####################################################################################################
 # Test 4
 ####################################################################################################
 
 func _setup_test_4() -> void:
 	var series_names := PackedStringArray(["A", "B"])
-	var x := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var x := PackedFloat64Array([0.0, NAN, 1.0, INF, 2.0, 2.5])
 	var y_a := PackedFloat64Array([1.0, 2.0, 1.4, 2.2, 1.1, 1.8])
-	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 1.2])
+	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 0.9])
 
 	var dataset := TauPlot.Dataset.make_shared_x_continuous(series_names, x, [y_a, y_b])
-	_datasets.append(dataset)
 
-	%TestPlot4.title = "[GROUPED] z-order = REVERSE_SERIES_ORDER"
+	%TestPlot4.title = "[INDEPENDENT + SHARED_X] 2nd and 4th x values are invalid"
 
 	var x_axis := TauAxisConfig.new()
-	x_axis.title = "X"
 	x_axis.type = TauAxisConfig.Type.CONTINUOUS
 	x_axis.scale = TauAxisConfig.Scale.LINEAR
 	x_axis.tick_count_preferred = 8
 
-	var left_axis := TauAxisConfig.new()
-	left_axis.title = "Y"
-	left_axis.type = TauAxisConfig.Type.CONTINUOUS
-	left_axis.scale = TauAxisConfig.Scale.LINEAR
-	left_axis.include_zero_in_domain = true
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
 
 	var bar_config := TauBarConfig.new()
-	bar_config.mode = TauBarConfig.BarMode.GROUPED
-	bar_config.z_order = TauPaneOverlayConfig.ZOrder.REVERSE_SERIES_ORDER
+	bar_config.mode = TauBarConfig.BarMode.INDEPENDENT
 	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
-	bar_config.bar_width_x_units = 0.25
-	bar_config.bar_gap_x_units = 0.0
+	bar_config.bar_width_x_units = 0.1
 
 	var pane := TauPaneConfig.new()
-	pane.y_left_axis = left_axis
+	pane.y_left_axis = y_axis
 	pane.overlays = [bar_config]
 
 	var config := TauXYConfig.new()
@@ -312,21 +220,6 @@ func _setup_test_4() -> void:
 
 	%TestPlot4.plot_xy(dataset, config, bindings)
 
-	_state.append(
-		{
-			"plot": %TestPlot4,
-			"bar_config": bar_config
-		}
-	)
-
-func _step_test_4() -> void:
-	var st := _state[3]
-	var plot: Control = st["plot"]
-	var bar_config: TauBarConfig = st["bar_config"]
-
-	bar_config.bar_width_x_units = clampf(0.25 + 0.25 * sin(_t * 0.5), 0.0, 0.5)
-	plot.refresh_now()
-
 ####################################################################################################
 # Test 5
 ####################################################################################################
@@ -334,39 +227,35 @@ func _step_test_4() -> void:
 func _setup_test_5() -> void:
 	var series_names := PackedStringArray(["A", "B"])
 	var x := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
-	var y_a := PackedFloat64Array([1.0, 2.0, 1.4, 2.2, 1.1, 1.8])
+	var y_a := PackedFloat64Array([1.0, NAN, 1.4, INF, 1.1, 1.8])
 	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 1.2])
 
 	var dataset := TauPlot.Dataset.make_shared_x_continuous(series_names, x, [y_a, y_b])
-	_datasets.append(dataset)
 
-	%TestPlot5.title = "[STACKED] z-order = REVERSE_SERIES_ORDER"
+	%TestPlot5.title = "[GROUPED] 2nd and 4th y values are invalid for series A"
 
 	var x_axis := TauAxisConfig.new()
-	x_axis.title = "X"
 	x_axis.type = TauAxisConfig.Type.CONTINUOUS
 	x_axis.scale = TauAxisConfig.Scale.LINEAR
 	x_axis.tick_count_preferred = 8
 
-	var left_axis := TauAxisConfig.new()
-	left_axis.title = "Y"
-	left_axis.type = TauAxisConfig.Type.CONTINUOUS
-	left_axis.scale = TauAxisConfig.Scale.LINEAR
-	left_axis.include_zero_in_domain = true
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
 
 	var bar_config := TauBarConfig.new()
-	bar_config.mode = TauBarConfig.BarMode.STACKED
-	bar_config.z_order = TauPaneOverlayConfig.ZOrder.REVERSE_SERIES_ORDER
+	bar_config.mode = TauBarConfig.BarMode.GROUPED
 	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
-	bar_config.bar_width_x_units = 0.4
+	bar_config.bar_width_x_units = 0.1
 
-	var pane := TauPaneConfig.new()
-	pane.y_left_axis = left_axis
-	pane.overlays = [bar_config]
+	var pane_config := TauPaneConfig.new()
+	pane_config.y_left_axis = y_axis
+	pane_config.overlays = [bar_config]
 
 	var config := TauXYConfig.new()
 	config.x_axis = x_axis
-	config.panes = [pane]
+	config.panes = [pane_config]
 	config.style.series_alpha = 0.8
 
 	var sb_a := TauXYSeriesBinding.new()
@@ -383,57 +272,37 @@ func _setup_test_5() -> void:
 
 	%TestPlot5.plot_xy(dataset, config, bindings)
 
-	_state.append(
-		{
-			"plot": %TestPlot5,
-			"bar_config": bar_config
-		}
-	)
-
-func _step_test_5() -> void:
-	var st := _state[4]
-	var plot: Control = st["plot"]
-	var bar_config: TauBarConfig = st["bar_config"]
-
-	bar_config.bar_width_x_units = clampf(0.4 + 0.4 * sin(_t * 0.6), 0.0, 0.8)
-	plot.refresh_now()
-
 ####################################################################################################
 # Test 6
 ####################################################################################################
 
 func _setup_test_6() -> void:
 	var series_names := PackedStringArray(["A", "B"])
-	var x_a := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
-	var x_b := PackedFloat64Array([0.25, 0.75, 1.25, 1.75, 2.25])
-	var y_a := PackedFloat64Array([1.0, 2.0, 1.4, 2.2, 1.1, 1.8])
-	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5])
+	var x := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var y_a := PackedFloat64Array([1.0, NAN, 1.4, INF, 1.1, 1.8])
+	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 1.2])
 
-	var dataset := TauPlot.Dataset.make_per_series_x_continuous(series_names, [x_a, x_b], [y_a, y_b])
-	_datasets.append(dataset)
+	var dataset := TauPlot.Dataset.make_shared_x_continuous(series_names, x, [y_a, y_b])
 
-	%TestPlot6.title = "[INDEPENDENT] z-order = REVERSE_SERIES_ORDER"
+	%TestPlot6.title = "[STACKED] 2nd and 4th y values are invalid for series A"
 
 	var x_axis := TauAxisConfig.new()
-	x_axis.title = "X"
 	x_axis.type = TauAxisConfig.Type.CONTINUOUS
 	x_axis.scale = TauAxisConfig.Scale.LINEAR
 	x_axis.tick_count_preferred = 8
 
-	var left_axis := TauAxisConfig.new()
-	left_axis.title = "Y"
-	left_axis.type = TauAxisConfig.Type.CONTINUOUS
-	left_axis.scale = TauAxisConfig.Scale.LINEAR
-	left_axis.include_zero_in_domain = true
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
 
 	var bar_config := TauBarConfig.new()
-	bar_config.mode = TauBarConfig.BarMode.INDEPENDENT
-	bar_config.z_order = TauPaneOverlayConfig.ZOrder.REVERSE_SERIES_ORDER
+	bar_config.mode = TauBarConfig.BarMode.STACKED
 	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
-	bar_config.bar_width_x_units = 0.25
+	bar_config.bar_width_x_units = 0.4
 
 	var pane := TauPaneConfig.new()
-	pane.y_left_axis = left_axis
+	pane.y_left_axis = y_axis
 	pane.overlays = [bar_config]
 
 	var config := TauXYConfig.new()
@@ -455,17 +324,107 @@ func _setup_test_6() -> void:
 
 	%TestPlot6.plot_xy(dataset, config, bindings)
 
-	_state.append(
-		{
-			"plot": %TestPlot6,
-			"bar_config": bar_config
-		}
-	)
+####################################################################################################
+# Test 7
+####################################################################################################
 
-func _step_test_6() -> void:
-	var st := _state[5]
-	var plot: Control = st["plot"]
-	var bar_config: TauBarConfig = st["bar_config"]
+func _setup_test_7() -> void:
+	var series_names := PackedStringArray(["A", "B"])
+	var x_a := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var x_b := PackedFloat64Array([0.25, 0.75, 1.25, 1.75, 2.25])
+	var y_a := PackedFloat64Array([1.0, NAN, 1.4, INF, 1.1, 1.8])
+	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5])
 
-	bar_config.bar_width_x_units = clampf(0.25 + 0.25 * sin(_t * 0.6), 0.0, 0.5)
-	plot.refresh_now()
+	var dataset := TauPlot.Dataset.make_per_series_x_continuous(series_names, [x_a, x_b], [y_a, y_b])
+
+	%TestPlot7.title = "[INDEPENDENT + PER_SERIES_X] 2nd and 4th y values are invalid for series A"
+
+	var x_axis := TauAxisConfig.new()
+	x_axis.type = TauAxisConfig.Type.CONTINUOUS
+	x_axis.scale = TauAxisConfig.Scale.LINEAR
+	x_axis.tick_count_preferred = 8
+
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
+
+	var bar_config := TauBarConfig.new()
+	bar_config.mode = TauBarConfig.BarMode.INDEPENDENT
+	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
+	bar_config.bar_width_x_units = 0.1
+
+	var pane := TauPaneConfig.new()
+	pane.y_left_axis = y_axis
+	pane.overlays = [bar_config]
+
+	var config := TauXYConfig.new()
+	config.x_axis = x_axis
+	config.panes = [pane]
+	config.style.series_alpha = 0.8
+
+	var sb_a := TauXYSeriesBinding.new()
+	sb_a.series_id = dataset.get_series_id_by_index(0)
+	sb_a.overlay_type = TauXYSeriesBinding.PaneOverlayType.BAR
+	sb_a.y_axis_id = TauPlot.AxisId.LEFT
+
+	var sb_b := TauXYSeriesBinding.new()
+	sb_b.series_id = dataset.get_series_id_by_index(1)
+	sb_b.overlay_type = TauXYSeriesBinding.PaneOverlayType.BAR
+	sb_b.y_axis_id = TauPlot.AxisId.LEFT
+
+	var bindings: Array[TauXYSeriesBinding] = [sb_a, sb_b]
+
+	%TestPlot7.plot_xy(dataset, config, bindings)
+
+####################################################################################################
+# Test 8
+####################################################################################################
+
+func _setup_test_8() -> void:
+	var series_names := PackedStringArray(["A", "B"])
+	var x := PackedFloat64Array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
+	var y_a := PackedFloat64Array([1.0, NAN, 1.4, INF, 1.1, 1.8])
+	var y_b := PackedFloat64Array([1.5, 1.0, 1.6, 2.4, 0.5, 0.9])
+
+	var dataset := TauPlot.Dataset.make_shared_x_continuous(series_names, x, [y_a, y_b])
+
+	%TestPlot8.title = "[INDEPENDENT + SHARED_X] 2nd and 4th y values are invalid for series A"
+
+	var x_axis := TauAxisConfig.new()
+	x_axis.type = TauAxisConfig.Type.CONTINUOUS
+	x_axis.scale = TauAxisConfig.Scale.LINEAR
+	x_axis.tick_count_preferred = 8
+
+	var y_axis := TauAxisConfig.new()
+	y_axis.type = TauAxisConfig.Type.CONTINUOUS
+	y_axis.scale = TauAxisConfig.Scale.LINEAR
+	y_axis.include_zero_in_domain = true
+
+	var bar_config := TauBarConfig.new()
+	bar_config.mode = TauBarConfig.BarMode.INDEPENDENT
+	bar_config.bar_width_policy = TauBarConfig.BarWidthPolicy.DATA_UNITS
+	bar_config.bar_width_x_units = 0.1
+
+	var pane := TauPaneConfig.new()
+	pane.y_left_axis = y_axis
+	pane.overlays = [bar_config]
+
+	var config := TauXYConfig.new()
+	config.x_axis = x_axis
+	config.panes = [pane]
+	config.style.series_alpha = 0.8
+
+	var sb_a := TauXYSeriesBinding.new()
+	sb_a.series_id = dataset.get_series_id_by_index(0)
+	sb_a.overlay_type = TauXYSeriesBinding.PaneOverlayType.BAR
+	sb_a.y_axis_id = TauPlot.AxisId.LEFT
+
+	var sb_b := TauXYSeriesBinding.new()
+	sb_b.series_id = dataset.get_series_id_by_index(1)
+	sb_b.overlay_type = TauXYSeriesBinding.PaneOverlayType.BAR
+	sb_b.y_axis_id = TauPlot.AxisId.LEFT
+
+	var bindings: Array[TauXYSeriesBinding] = [sb_a, sb_b]
+
+	%TestPlot8.plot_xy(dataset, config, bindings)
